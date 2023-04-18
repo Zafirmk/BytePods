3f0072465709794027e29fbda0df909b04317e0d
Zafirmk <zafirmk0@gmail.com>
2023-04-18 03:01:53 -0400

    Complete SummarizeNews v1

--- Utils/SummarizeNews.py
+++ Utils/SummarizeNews.py
@@ -1,4 +1,27 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
 # Summarize news articles -> GeneratePodcast
 class SummarizeNews:
-    def __init__(self) -> None:
-        pass
+    
+    def __init__(self, articles) -> None:
+        self.summaries = []
+        self.articles = articles
+
+    def summarizeArticles(self):
+
+        for article in self.articles:
+            response = openai.ChatCompletion.create(
+                model = 'gpt-3.5-turbo',
+                messages = [
+                    {"role": "system", "content": "You summarize news articles into a script for a podcast."},
+                    {"role": "user", "content": os.getenv('OPENAI_PROMPT') + str(article[2])}
+                ],
+                temperature = 0.0
+            )
+
+            self.summaries.append(response)
