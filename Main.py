1f952f96890a586bfb7e2e636c5cccb31e0470bf
Zafirmk <zafirmk0@gmail.com>
2023-04-20 01:14:04 -0400

    Successfully generates podcast to GCP bucket

--- Main.py
+++ Main.py
@@ -1,4 +1,12 @@
-from Utils import ArticleScrapper, NewsExtractor, SummarizeNews, GeneratePodcast, PublishPodcast
+from Utils.ArticleScrapper import ArticleScrapper
+from Utils.SummarizeNews import SummarizeNews
+from Utils.GeneratePodcast import GeneratePodcast
 
 if __name__ == '__main__':
-    pass
+    news_reports = ArticleScrapper('http://www.ground.news').getNews()
+    print('Article Scrapper Done\n')
+    summaries = SummarizeNews(news_reports)
+    summaries.summarizeArticles()
+    print('Summarizer Done\n')
+    GeneratePodcast(summaries.getSummaries())
+    print('Podcast Generated\n')
