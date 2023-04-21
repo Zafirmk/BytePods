0b82c51a668916c5da471939cb1d0eeb9c5bdf6c
Zafirmk <zafirmk0@gmail.com>
2023-04-21 01:26:01 -0400

    Generates description for podcasts

--- Utils/SummarizeNews.py
+++ Utils/SummarizeNews.py
@@ -33,10 +33,32 @@ class SummarizeNews:
 
         self.summaries = [s for s in self.summaries if s != ""]
         self.logSummaries()
+        self.generateDescription()
 
     def logSummaries(self):
         blob = self.bucket.blob('logs/log_summaries.txt')
         blob.upload_from_string('\n'.join(self.summaries))
+
+    def generateDescription(self):
+        headlines = list(map(lambda string: string.split(".")[0], self.summaries[:3]))
+        
+        try:
+            response = openai.ChatCompletion.create(
+                model = 'gpt-3.5-turbo',
+                messages = [
+                    {"role": "system", "content": "You create a description for a podcast."},
+                    {"role": "user", "content": os.getenv('OPENAI_DESCRIPTION_PROMPT') + str(headlines[0]) + '\n' + str(headlines[1]) + '\n' + str(headlines[2]) + '\n'}
+                ],
+                temperature = 0.0
+            )
+
+            blob = self.bucket.blob('description.txt')
+            blob.upload_from_string(response["choices"][0]["message"]["content"])
+
+        except:
+            response = "Welcome to today's episode! Have a nice day ahead!"
+            blob = self.bucket.blob('description.txt')
+            blob.upload_from_string(response)
 
     def getSummaries(self):
         return self.summaries
