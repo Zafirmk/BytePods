d28f5120d0ce785a99c5d5c4c748cca4f57563e2
Zafirmk <zafirmk0@gmail.com>
2023-04-24 21:47:24 -0400

    Pylint changes

--- Main.py
+++ Main.py
@@ -10,12 +10,12 @@ def main():
     """
     Main function that executes the NewsBytes pipeline.
     """
-    news_reports = ArticleScrapper('http://www.ground.news').getNews()
+    news_reports = ArticleScrapper('http://www.ground.news').get_news()
     print('Article Scrapper Done\n')
     summaries = SummarizeNews(news_reports)
-    summaries.summarizeArticles()
+    summaries.summarize_articles()
     print('Summarizer Done\n')
-    episode_name = GeneratePodcast(summaries.getSummaries()).getEpisodeName()
+    episode_name = GeneratePodcast(summaries.get_summaries()).get_episode_name()
     print('Podcast Generated\n')
     PublishPodcast(episode_name)
     print('Podcast Published\n')
