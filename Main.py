bbf2c4fb038b645cf56d8b0e9901b3bae0c17967
Zafirmk <zafirmk0@gmail.com>
2023-05-09 01:48:29 +0400

    Initial commit

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # pylint: disable=C0103
 """
-Entry point of NewsBytes pipeline.
+Entry point of CryptoBytes pipeline.
 """
 from Utils.ArticleScrapper import ArticleScrapper
 from Utils.SummarizeNews import SummarizeNews
@@ -9,17 +9,8 @@ from Utils.PublishPodcast import PublishPodcast
 
 def main():
     """
-    Main function that executes the NewsBytes pipeline.
+    Main function that executes the CryptoBytes pipeline.
     """
-    news_reports = ArticleScrapper('http://www.ground.news').get_news()
-    print('Article Scrapper Done\n')
-    summaries = SummarizeNews(news_reports)
-    summaries.summarize_articles()
-    print('Summarizer Done\n')
-    episode_name = GeneratePodcast(summaries.get_summaries()).get_episode_name()
-    print('Podcast Generated\n')
-    PublishPodcast(episode_name)
-    print('Podcast Published\n')
 
 if __name__ == '__main__':
     main()
